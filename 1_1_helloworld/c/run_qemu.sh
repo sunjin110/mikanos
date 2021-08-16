f6e9254fdc0045d4230bc52815257d40e1712485
yun_sunjin <yun_sunjin@cocone.co.jp>
2021-08-16 14:32:34 +0900

    a

--- 1_1_helloworld/c/run_qemu.sh
+++ 1_1_helloworld/c/run_qemu.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
+EFI_FILE=BOOTX64.EFI
 DISK_IMG=./disk.img
+MOUNT_POINT=./mnt
 
 # make image
+rm -f $DISK_IMG
 qemu-img create -f raw $DISK_IMG 200M
 mkfs.fat -n 'MIKAN OS' -s 2 -f 2 -R 32 -F 32 $DISK_IMG
 
 # mount image
+mkdir -p $MOUNT_POINT
+sudo mount -o loop $DISK_IMG $MOUNT_POINT
+
+# 
+
+# 起動
+qemu-system-x86_64 -m 1G -drive -if=pflash
