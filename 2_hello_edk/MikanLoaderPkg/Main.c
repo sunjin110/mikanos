9bee1034540365fabff2bd7a1657c2aa5cdde61c
yun_sunjin <sunjin110@gmail.com>
2021-09-12 01:44:58 +0900

    a

--- 2_hello_edk/MikanLoaderPkg/Main.c
+++ 2_hello_edk/MikanLoaderPkg/Main.c
@@ -7,49 +7,45 @@
 #include <Protocol/DiskIo2.h>
 #include <Protocol/BlockIo.h>
 
+// MemoryMap 独自定義のメモリマップ
 struct MemoryMap {
     UINTN buffer_size;
-    VOID* buffer;
+    VOID* buffer; // void*はどのような型でもpointerであれば受け取れるというもの
     UINTN map_size;
     UINTN map_key;
     UINTN descriptor_size;
     UINT32 descriptor_version;
 };
 
-// GetMemoryMap メモリマップを取得する
+// GetMemoryMap memory_mapの状態をあれする
 EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
-
+    // bufferがない場合は、ない
     if (map->buffer == NULL) {
         return EFI_BUFFER_TOO_SMALL;
     }
-
-    
     map->map_size = map->buffer_size;
-    return gBs->GetMemoryMap(
-        &map->map_size,
-        (EFI_MEMORY_DESCRIPTOR*)map->beffer,
-        &map->map_key,
-        &map->descriptor_size,
-        &map->descriptor_version);
+    return gBS->GetMemoryMap(&map->map_size, (EFI_MEMORY_DESCRIPTOR*)map->buffer, &map->map_key, &map->descriptor_size, &map->descriptor_version);
 }
 
-const CHAR16* GetMemoryTypeUnicode(EFI_MEMORY_TYPE type) {
-    switch (type) {
-        case EfiReservedMemoryType: return L"EfiReservedMemoryType";
-        case EfiLoaderCode: return L"EfiLoaderCode";
-        case EfiLoaderData: return L"EfiLoaderData";
-        case EfiBootServicesCode: return L"EfiBootServicesCode";
-        case EfiBootServicesData: return L"EfiBootServicesData";
-        case EfiRuntimeServicesCode: return L"EfiRuntimeServicesCode";
-        case EfiRuntimeServicesData: return L"EfiRuntimeServicesData";
-        case EfiConventionalMemory: return L"EfiConventionalMemory";
-        case EfiUnusableMemory: return L"EfiUnusableMemory";
-        case EfiACPIReclaimMemory: return L"EfiACPIReclaimMemory";
-        case EfiACPIMemoryNVS: return L"EfiACPIMemoryNVS";
-        case EfiMemoryMappedIO: return L"EfiMemoryMappedIO";
-        case EfiMemoryMappedIOPortSpace: return L"EfiMemoryMappedIOPortSpace";
-        case EfiPalCode: return L"EfiPalCode";
-        case EfiPersistentMemory: return L"EfiPersistentMemory";
-        case EfiMaxMemoryType: return L"EfiMaxMemoryType";
-        default: return L"InvalidMemoryType";
+
+// Main
+EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* system_table) {
+
+    // hello world
+    Print(L"Hello, Mikan Sunjin World!");
+
+    // MemoryMapの取得
+    CHAR8 memmap_buf[4096 * 4];
+    struct MemoryMap memmap = {
+        buffer_size: sizeof(memmap_buf),
+        buffer: memmap_buf,
+        map_size: 0,
+        map_key: 0,
+        descriptor_size: 0,
+        descriptor_version: 0,
+    };
+
+    
+    
+
 }
