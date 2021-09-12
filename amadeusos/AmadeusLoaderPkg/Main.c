10258fba0c1d446436c34107f36c88c637ca69c0
yun_sunjin <sunjin110@gmail.com>
2021-09-12 22:17:51 +0900

    a

--- amadeusos/AmadeusLoaderPkg/Main.c
+++ amadeusos/AmadeusLoaderPkg/Main.c
@@ -1,1 +1,159 @@
-../../2_hello_edk/MikanLoaderPkg/Main.c
+#include  <Uefi.h>
+#include  <Library/UefiLib.h>
+#include  <Library/UefiBootServicesTableLib.h>
+#include  <Library/PrintLib.h>
+#include  <Protocol/LoadedImage.h>
+#include  <Protocol/SimpleFileSystem.h>
+#include  <Protocol/DiskIo2.h>
+#include  <Protocol/BlockIo.h>
+
+// MemoryMap 独自定義のメモリマップ
+struct MemoryMap {
+    UINTN buffer_size;
+    VOID* buffer; // void*はどのような型でもpointerであれば受け取れるというもの
+    UINTN map_size;
+    UINTN map_key;
+    UINTN descriptor_size;
+    UINT32 descriptor_version;
+};
+
+// GetMemoryMap memory_mapの状態をあれする
+EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
+    // bufferがない場合は、ない
+    if (map->buffer == NULL) {
+        return EFI_BUFFER_TOO_SMALL;
+    }
+    map->map_size = map->buffer_size;
+    return gBS->GetMemoryMap(&map->map_size, (EFI_MEMORY_DESCRIPTOR*)map->buffer, &map->map_key, &map->descriptor_size, &map->descriptor_version);
+}
+
+// OpenRootDir rootのディレクトリを開く
+EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL** root) {
+    EFI_LOADED_IMAGE_PROTOCOL* loaded_image;
+    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* fs;
+
+    gBS->OpenProtocol(
+        image_handle,
+        &gEfiLoadedImageProtocolGuid,
+        (VOID**)&loaded_image,
+        image_handle,
+        NULL,
+        EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+
+    gBS->OpenProtocol(
+        loaded_image->DeviceHandle,
+        &gEfiSimpleFileSystemProtocolGuid,
+        (VOID**)&fs,
+        image_handle,
+        NULL,
+        EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+
+    fs->OpenVolume(fs, root);
+
+    return EFI_SUCCESS;
+}
+
+
+const CHAR16* GetMemoryTypeUnicode(EFI_MEMORY_TYPE type) {
+    switch (type)
+    {
+    case EfiReservedMemoryType: return L"EfiReservedMemoryType";
+    case EfiLoaderCode: return L"EfiLoaderCode";
+    case EfiLoaderData: return L"EfiLoaderData";
+    case EfiBootServicesCode: return L"EfiBootServicesCode";
+    case EfiBootServicesData: return L"EfiBootServicesData";
+    case EfiRuntimeServicesCode: return L"EfiRuntimeServicesCode";
+    case EfiRuntimeServicesData: return L"EfiRuntimeServicesData";
+    case EfiConventionalMemory: return L"EfiConventionalMemory";
+    case EfiUnusableMemory: return L"EfiUnusableMemory";
+    case EfiACPIReclaimMemory: return L"EfiACPIReclaimMemory";
+    case EfiACPIMemoryNVS: return L"EfiACPIMemoryNVS";
+    case EfiMemoryMappedIO: return L"EfiMemoryMappedIO";
+    case EfiMemoryMappedIOPortSpace: return L"EfiMemoryMappedIOPortSpace";
+    case EfiPalCode: return L"EfiPalCode";
+    case EfiPersistentMemory: return L"EfiPersistentMemory";
+    case EfiMaxMemoryType: return L"EfiMaxMemoryType";
+    default:
+        return L"InvalidMemoryType";
+    }
+}
+
+// SaveMemoryMap メモリマップをファイルに保存する
+EFI_STATUS SaveMemoryMap(struct MemoryMap* map, EFI_FILE_PROTOCOL* file) {
+
+    CHAR8 buf[256];
+    UINTN len;
+
+    CHAR8* header = "Index, Type, Type(name), PhysicalStart, NumberOfPages, Attribute\n";
+    len = AsciiStrLen(header);
+    file->Write(file, &len, header);
+
+    Print(L"map->buffer = %08lx, map->map_size = %08lx\n", map->buffer, map->map_size);
+
+    EFI_PHYSICAL_ADDRESS iter; // 指し記したいメモリマップのアドレスを整数でiterする
+    int i;
+    for (iter = (EFI_PHYSICAL_ADDRESS)map->buffer, i = 0; iter < (EFI_PHYSICAL_ADDRESS)map->buffer + map->map_size; iter += map->descriptor_size, i++) {
+
+        EFI_MEMORY_DESCRIPTOR* desc = (EFI_MEMORY_DESCRIPTOR*)iter; // iterを実際のメモリの場所を指すpointerに変換する
+
+        len = AsciiSPrint(
+            buf, 
+            sizeof(buf), 
+            "%u, %x, %-ls, %08lx, %lx, %lx\n", 
+            i, 
+            desc->Type, 
+            GetMemoryTypeUnicode(desc->Type),
+            desc->PhysicalStart,
+            desc->NumberOfPages,
+            desc->Attribute & 0xffffflu);
+
+        file->Write(file, &len, buf);
+    }
+    return EFI_SUCCESS;
+}
+
+// Main
+EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* system_table) {
+
+    // hello world
+    Print(L"Hello, AmadeusOS World!\n");
+
+    // MemoryMapの取得
+    CHAR8 memmap_buf[4096 * 4];
+    struct MemoryMap memmap = {
+        sizeof(memmap_buf),
+        memmap_buf,
+        0,
+        0,
+        0,
+        0,
+    };
+    GetMemoryMap(&memmap);
+
+    Print(L"GetMemoryMap ok\n");
+
+    // 書き込み先のファイルを開く
+    EFI_FILE_PROTOCOL* root_dir;
+    OpenRootDir(image_handle, &root_dir);
+
+    Print(L"OpenRootDir ok\n");
+
+
+    EFI_FILE_PROTOCOL* memmap_file;
+    root_dir->Open(
+       root_dir, &memmap_file, L"\\memmap",
+       EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE, 0);
+
+    Print(L"file open ok\n");
+
+    SaveMemoryMap(&memmap, memmap_file);
+
+    Print(L"Save ok\n");
+
+    memmap_file->Close(memmap_file);
+
+    Print(L"All done!!!\n");
+
+    while (1);
+    return EFI_SUCCESS;
+}
