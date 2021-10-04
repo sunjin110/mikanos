3228b31bb3dc7de34cbff06fac0fa2a79a920491
yun_sunjin <sunjin110@gmail.com>
2021-10-05 00:19:17 +0900

    a

--- amadeusos/build_kernel.sh
+++ amadeusos/build_kernel.sh
@@ -6,6 +6,8 @@ cd kernel
 # ld.lld --entry KernelMain -z norelro --image-base 0x100000 --static -o kernel.elf main.o
 
 
-clang++ $CPPFLAGS -O2 -Wall -g --target=x86_64-efl -ffreestanding -mno-red-zone -fno-exceptions -fno-rtti -std=c++17 -c main.cpp
+# clang++ $CPPFLAGS -O2 -Wall -g --target=x86_64-efl -ffreestanding -mno-red-zone -fno-exceptions -fno-rtti -std=c++17 -c main.cpp
+
+clang++ $CPPFLAGS -O2 --target=x86_64-elf -fno-exceptions -ffreestanding -c main.cpp
 
 ld.lld $LDFLAGS --entry KernelMain -z norelro --image-base 0x100000 --static -o kernel.elf main.o
